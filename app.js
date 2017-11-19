4cedee916b731e1e40b353737b61d7ac1b793341
Benny Skogberg <benny.skogberg@gmail.com>
2017-11-19 21:36:05 +0100

    Removing the host from server.js (doesn't display anything). Make sure to use valid names in package.json script tag. start and test are valid, server and app are not.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'})
 
   // Send the response body as 'Hello World'
-  response.end('Hello World\n')
+  response.end('Hello World from app.js\n')
 }).listen(4000)
 
 // Console will print the message
--- src/server.js
+++ src/server.js
@@ -2,12 +2,11 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World from server.js')
 })
 
 var server = app.listen(4000, function () {
-  var host = server.address().address
   var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Example app listening at http://localhost:%s', port)
 })
